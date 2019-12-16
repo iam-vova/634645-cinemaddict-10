a54fcb525df97678483f4faff428c036943ae4bd
iam-vova <iam-vova@yandex.ru>
2019-12-17 00:09:04 +0300

    add films-extra component

--- src/components/films.js
+++ src/components/films.js
@@ -10,20 +10,6 @@ const createFilmsTemplate = () => {
         </div>
         
       </section>
-  
-      <section class="films-list--extra">
-        <h2 class="films-list__title">Top rated</h2>
-  
-        <div class="films-list__container">
-        </div>
-      </section>
-  
-      <section class="films-list--extra">
-        <h2 class="films-list__title">Most commented</h2>
-  
-        <div class="films-list__container">
-        </div>
-      </section>
     </section>`
   );
 };
