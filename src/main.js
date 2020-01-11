6498fffc20e833925cd854d015185a46f3fde987
iam-vova <iam-vova@yandex.ru>
2020-01-11 19:49:47 +0300

    add filter-controller

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import SiteMenu from './components/site-menu.js';
+// import SiteMenu from './components/site-menu.js';
 import UserRate from './components/user-rate.js';
 import Films from './components/films.js';
-import FilmsModel from './models/movies.js'
+import FilmsModel from './models/movies.js';
 import PageController from './controllers/page-controller.js';
+import FilterController from './controllers/filter-controller.js';
 import {generateFilmCards} from './mock/film-card.js';
 import {render, RenderPosition} from './utils/render.js';
+import {getFilmsByFilter} from "./utils/filter.js";
+import {FilterType} from "./const.js";
 
 const FILMS_CARDS_COUNT = 15;
 
@@ -13,10 +16,12 @@ const filmsModel = new FilmsModel();
 filmsModel.setFilms(films);
 
 const siteMainElement = document.querySelector(`.main`);
-render(siteMainElement, new SiteMenu(films), RenderPosition.BEFOREEND);
+const filterController = new FilterController(siteMainElement, filmsModel);
+filterController.render();
 
 const siteHeaderElement = document.querySelector(`.header`);
-render(siteHeaderElement, new UserRate(films), RenderPosition.BEFOREEND);
+const watchedFilms = getFilmsByFilter(filmsModel.getFilmsAll(), FilterType.HISTORY).length;
+render(siteHeaderElement, new UserRate(watchedFilms), RenderPosition.BEFOREEND);
 
 const filmsComponent = new Films();
 render(siteMainElement, filmsComponent, RenderPosition.BEFOREEND);
--- src/mock/filter.js
+++
@@ -1,3 +0,0 @@
-export const filterCount = (films, parametr) => {
-  return films.filter((it) => it[parametr] === true).length;
-};
